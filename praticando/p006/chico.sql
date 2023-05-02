5e7043012c105923b5f6d739a1da754c50243c68
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-05-02 17:26:50 -0300

    Create chico.sql

---
+++ praticando/p006/chico.sql
@@ -0,0 +1,35 @@
+-- STUDENT
+
+CREATE TABLE COURSE (
+  course_number VARCHAR(8) PRIMARY KEY,
+  course_name VARCHAR(25) NOT NULL UNIQUE,
+  department VARCHAR(4) NOT NULL,
+  credit_hours CHAR(1) NOT NULL
+);
+
+CREATE TABLE SECTION (
+  section_identifier VARCHAR(3) PRIMARY KEY,
+  course_number VARCHAR(8) FOREIGN KEY,
+  semester VARCHAR(6) NOT NULL,
+  year CHAR(2) NOT NULL,
+  instructor VARCHAR(8) NOT NULL
+);
+
+CREATE TABLE STUDENT (
+  student_number VARCHAR(2) PRIMARY KEY,
+  name VARCHAR(10) NOT NULL,
+  class CHAR(1) NOT NULL,
+  major CHAR(2)
+);
+
+CREATE TABLE GRADE_REPORT (
+  student_number VARCHAR(3) PRIMARY KEY,
+  section_identifier VARCHAR(3) PRIMARY KEY,
+  grade CHAR(1) NOT NULL
+);
+
+CREATE TABLE PREREQUISITE (
+  prerequisite_number VARCHAR(8) PRIMARY KEY,
+  course_number VARCHAR(6) NOT NULL
+);
+
